d1652b525b618628d9569b7e4fd810b49bdd14c0
SARI MOHAND <mohandsari@outlook.fr>
2023-04-24 11:48:49 +0200

    vue init

---
+++ frontend/src/components/BookCrud.vue
@@ -0,0 +1,15 @@
+<template>
+
+  //design
+
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
